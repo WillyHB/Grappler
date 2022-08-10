f29884d8c8026194495dfe04bcdc13149c515c81
WillyHB <william.hansen.baird@icloud.com>
2022-08-10 15:23:55 +0200

    Added Connection rope & new art

---
+++ Assets/Scripts/Ropes/ConnectionRope.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionRope : Rope
+{
+    public Rigidbody2D ConnectedBody;
+
+    private DistanceJoint2D distanceJoint;
+
+    void Start()
+    {
+        distanceJoint = gameObject.AddComponent<DistanceJoint2D>();
+        distanceJoint.connectedBody = ConnectedBody;
+        distanceJoint.maxDistanceOnly = true;
+
+        lineRenderer = GetComponent<LineRenderer>();
+
+        ResetRope(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SegmentFrequency != segmentFrequency || LineLength != lineLength)
+        {
+            ModifyLength(ConnectedBody.transform.position);
+            //ResetRope(StartPoint.position);
+        }
+
+        DrawRope();
+    }
+
+    public float GetDistanceBetweenBodies() => (transform.position - ConnectedBody.transform.position).magnitude;
+
+    private void FixedUpdate()
+    {
+        Simulate();
+
+        distanceJoint.distance = LineLength;
+    }
+
+    protected override void ApplyConstraint()
+    {
+        //Constrant to First Point 
+        RopeSegment firstSegment = ropeSegments[0];
+        firstSegment.posNow = transform.position;
+        ropeSegments[0] = firstSegment;
+
+        //Constrant to Second Point 
+        RopeSegment endSegment = ropeSegments[^1];
+        endSegment.posNow = ConnectedBody.transform.position;
+        ropeSegments[^1] = endSegment;
+
+        for (int i = 0; i < numberOfSegments - 1; i++)
+        {
+            RopeSegment firstSeg = ropeSegments[i];
+            RopeSegment secondSeg = ropeSegments[i + 1];
+
+            float dist = (firstSeg.posNow - secondSeg.posNow).magnitude;
+            float error = dist - lengthBetweenSegments;
+
+            Vector2 changeDir = (firstSeg.posNow - secondSeg.posNow).normalized;
+
+            Vector2 changeAmount = changeDir * error;
+
+            if (i != 0)
+            {
+                firstSeg.posNow -= changeAmount * 0.5f;
+                ropeSegments[i] = firstSeg;
+                secondSeg.posNow += changeAmount * 0.5f;
+                ropeSegments[i + 1] = secondSeg;
+            }
+            else
+            {
+                secondSeg.posNow += changeAmount;
+                ropeSegments[i + 1] = secondSeg;
+            }
+        }
+    }
+
+    private void DrawRope()
+    {
+        lineRenderer.startWidth = LineWidth;
+        lineRenderer.endWidth = LineWidth;
+
+        Vector3[] ropePositions = new Vector3[numberOfSegments];
+        for (int i = 0; i < numberOfSegments; i++)
+        {
+            ropePositions[i] = ropeSegments[i].posNow;
+        }
+
+        lineRenderer.positionCount = ropePositions.Length;
+        lineRenderer.SetPositions(ropePositions);
+    }
+}
--- Assets/Scripts/Ropes/Rope.cs
+++ Assets/Scripts/Ropes/Rope.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public abstract class Rope : MonoBehaviour
 {
     protected LineRenderer lineRenderer;
