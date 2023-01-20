7ebe2ce2289e2b3bc9be9a595b9d57252a886601
WillyHB <william.hansen.baird@icloud.com>
2023-01-20 23:17:19 +0100

    What am I to say? Continued environment

--- Assets/Scripts/StateMachine/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/PlayerStateMachine.cs
@@ -176,7 +176,9 @@ public class PlayerStateMachine : StateMachine
         Animator = GetComponent<Animator>();
 
         //transform.position = FindObjectOfType<RoomManager>().rooms[GameData.Load().Checkpoint].Checkpoint.position;
-        transform.position = FindObjectOfType<RoomManager>().rooms[12].Checkpoint.position;
+        transform.position = FindObjectOfType<RoomManager>().rooms[13].Checkpoint.position;
+
+        Transition(IdleState);
     }
 
     public void PlayFootstep()
